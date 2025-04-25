73c39090fdcb6c83d6a576efdbcf9bca3034edd5
dhp94d <dhp94d@gmail.com>
2025-04-25 09:36:51 +0900

    dhyun2(chore): add member & edit pr action
    * 멤버추가 eonhwakim
    
    * edit pr action
    리뷰어 목록 fe-security -> multiparadigm

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'], // Conventional Commits 확장
   rules: {
-    'type-enum': [2, 'always', ['dhyun2']], // GitHub ID 강제
+    'type-enum': [2, 'always', ['dhyun2', 'yonghyun421', 'dohye1', 'rae-han', 'wisdom08', '4anghyeon', 'eonhwakim']], // GitHub ID 강제
     'type-empty': [2, 'never'],
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
